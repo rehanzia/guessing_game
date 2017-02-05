f6e372414f97e61fbb38164092eba1ebb0ade96f
rehanzia <rehanzia91@gmail.com>
2017-02-04 16:20:39 -0800

    initial

---
+++ app.js
@@ -0,0 +1,47 @@
+//
+var userName = prompt('What\'s your name?');
+alert ('Hello ' + userName + '. Thanks for coming to my site!');
+//We want ask the user question
+
+
+
+//question 1
+var answerOne = prompt('what state was I born in?').toLowerCase();
+var myAnswerOne = 'new jersey';
+console.log(answerOne);
+if(answerOne === myAnswerOne){
+  alert('You got it right. I was born in New Jersey');
+} else {
+  alert('Sorry, that\'s wrong. I was born in New Jersey');
+}
+
+
+  //question 2
+  var answerOne = prompt('My favorite company is ______').toLowerCase();
+  var myAnswerOne = 'apple';
+  console.log(answerOne);
+  if(answerOne === myAnswerOne){
+    alert('You got it right. My favorite company is apple');
+  } else {
+    alert('Sorry, that\'s wrong. My favorite is apple');
+}
+
+    //question 3
+    var answerOne = prompt('').toLowerCase();
+    var myAnswerOne = 'new jersey';
+    console.log(answerOne);
+    if(answerOne === myAnswerOne){
+      alert('You got it right. I was born in New Jersey');
+    } else {
+      alert('Sorry, that\'s wrong. I was born in New Jersey');
+}
+
+      //question 4
+      var answerOne = prompt('what state was I born in?').toLowerCase();
+      var myAnswerOne = 'new jersey';
+      console.log(answerOne);
+      if(answerOne === myAnswerOne){
+        alert('You got it right. I was born in New Jersey');
+      } else {
+        alert('Sorry, that\'s wrong. I was born in New Jersey');
+      }
